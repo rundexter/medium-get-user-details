a779241eff42dbbddaeb93a631bd5f8275d1cd6d
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-06 15:55:16 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+var util = require('./util.js');
+var request = require('request').defaults({
+    baseUrl: 'https://api.medium.com/'
+});
+
+var pickOutputs = {
+        'id': 'data.id',
+        'username': 'data.username',
+        'name': 'data.name',
+        'url': 'data.url',
+        'imageUrl': 'data.imageUrl'
+    };
+
 module.exports = {
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +20,17 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var token = dexter.environment('medium_access_token'),
+            api = '/v1/me';
+
+        if (!token)
+            return this.fail('A [medium_access_token] environment variable is required for this module');
+
+        request.get({uri: api, auth: { bearer: token }, json: true}, function (error, response, body) {
+            if (error)
+                this.fail(error);
+            else
+                this.complete(util.pickOutputs(body, pickOutputs));
+        }.bind(this));
     }
 };
